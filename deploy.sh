abf5138f3e34816b6816fd9a7617da24d4adf350
Mario Carneiro <di.gama@gmail.com>
2016-08-15 20:15:26 -0400

    save and replay exit code from java

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,14 @@ TARGET_BRANCH="gh-pages"
 function doCompile {
   javac src/TestHarness.java -d .
   java TestHarness
+  test_exit_code=$?
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
     doCompile
-    exit 0
+    exit "$test_exit_code"
 fi
 
 # Save some useful information
@@ -44,7 +45,7 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if [ -z `git diff --exit-code` ]; then
     echo "No changes to the output on this push; exiting."
-    exit 0
+    exit "$test_exit_code"
 fi
 
 # Commit the "changes", i.e. the new version.
@@ -64,3 +65,4 @@ ssh-add deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
+exit "$test_exit_code"
